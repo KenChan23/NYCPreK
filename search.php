45260d8b14d1b1177a740d3aafa3bb7404aa1f3b
Alan <chen.alan101@gmail.com>
2014-05-31 15:45:57 -0400

    location

--- search.php
+++ search.php
@@ -1,2 +1,41 @@
-<?php
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<script
+src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDY0kkJiTPVd2U7aTOAwhc9ySH6oHxOIYM&sensor=true">
+</script>
+
+<script>
+var myCenter=new google.maps.LatLng(40.7651804,-73.98030080000001);
+var school = new google.maps.LatLng(40.71261399999999,-73.99730959999999);
+function initialize()
+{
+var mapProp = {
+  center:myCenter,
+  zoom:15,
+  mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+var marker=new google.maps.Marker({
+  position:myCenter,
+  });
+
+marker.setMap(map);
+
+var marker1=new google.maps.Marker({
+  position:school,
+  });
+
+marker1.setMap(map);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+</script>
+</head>
+
+<body>
+<div id="googleMap" style="width:500px;height:380px;"></div>
+</body>
+</html>
